cb444fa79f276950cec09e58a87f8b6d43ef3d06
edithknaab <kwadannaabe1@southernct.edu>
2024-03-03 18:48:38 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World - this is Edith's project </h1>
+    </div>
+  );
+}
+
+export default App;
