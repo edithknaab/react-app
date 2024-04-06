4cd2bd8b2564a65a9f0ab0780400770949e8c6eb
EC2 Default User <ec2-user@ip-172-31-6-151.us-east-2.compute.internal>
2024-04-06 23:05:56 +0000

    changes made to pythoin file

--- deploy.py
+++ deploy.py
@@ -1,34 +1,40 @@
+#import argparse and fabric to execute commans using python
 import argparse
 from fabric import Connection, Config
-
+#define function
 def deploy_to_ec2(repo_url):
-    # EC2 instance details
+    # ec2 instance ip, username, and key path
     ec2_host = '3.141.240.128'
     ec2_username = 'ec2-user'
-    ec2_key_path = 'csc497.pem'  # Update the path to the PEM file
+    #stored in desktop
+    ec2_key_path = 'csc497.pem' 
 
     try:
-        # Connect to EC2 instance
+        # connect to the ec2 instance
         conn = Connection(host=ec2_host, user=ec2_username, connect_kwargs={'key_filename': ec2_key_path})
 
-        # Commands to execute on EC2 instance
+        # commands used to execute ec2 instance connections
         with conn.cd('/home/ec2-user/react-app'):
-            conn.run('git pull {}'.format(repo_url))  # Pull latest changes from your Git repository
-            conn.run('npm install')             # Install any new dependencies
-            conn.run('npm run build')           # Build your React application
+	    #pull recent commits from github
+            conn.run('git pull {}'.format(repo_url)) 
+	    # npm install
+            conn.run('npm install')            
+            # build an optimized version of the app
+            conn.run('npm run build')          
+            # run the applicatino using pm2
             conn.sudo('pm2 restart react-app')
-
+	#if succeffsul print this message
         print("Deployment completed successfully!")
 
     except Exception as e:
         print("Error:", e)
-
+#initilize argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Deploy a React application to EC2.')
     parser.add_argument('repo_url', type=str, help='GitHub repository URL')
 
     args = parser.parse_args()
 
-    # Trigger deployment
+    # Trigger deployment funciotn
     deploy_to_ec2(args.repo_url)
 
