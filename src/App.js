8bf791d02978d9834b882721e6a87e04f75a7e2c
edithknaab <kwadannaabe1@southernct.edu>
2024-04-06 17:41:54 -0400

    checking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello World -Checking to see if deployment WORKED PLEASE WORK!!!! this is Edith's project </h1>
+      <h1>Chekcing againnnnnn!! Hello World -Checking to see if deployment WORKED PLEASE WORK!!!! this is Edith's project </h1>
     </div>
   );
 }
