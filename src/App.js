fb8af820d8f199791763499704e5d16d920771b4
edithknaab <kwadannaabe1@southernct.edu>
2024-04-28 05:17:53 +0000

    Revert "Remove nested Git repository and ignore react-app directory"
    This reverts commit b1504121a346f0ae5eeaa2899110c52c133b28c7.
    
    wwoudcno

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 
 function App() {
   return (
     <div>
-      <h1>Please work React App</h1>
+      <h1>React App</h1>
     </div>
   );
 }
