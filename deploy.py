9345a8dbfd674d5bfe6a42be92a90c25de895364
EC2 Default User <ec2-user@ip-172-31-34-51.us-east-2.compute.internal>
2024-05-09 17:10:25 +0000

    dedED

--- deploy.py
+++ deploy.py
@@ -1,13 +1,14 @@
 #import argparse and fabric to execute commands using python
 import argparse
 from fabric import Connection
+
 #define function
 def deploy_to_ec2(repo_url):
     # ec2 instance ip, username, and key path
-    ec2_host = '3.19.202.148'
+    ec2_host = '13.59.178.164'
     ec2_username = 'ec2-user'
     #stored in desktop
-    ec2_key_path = 'deploy.pem' 
+    ec2_key_path = 'team.pem' 
 
     try:
         # connect to the ec2 instance
@@ -15,6 +16,8 @@ def deploy_to_ec2(repo_url):
 
         # commands used to execute ec2 instance connections
         with conn.cd('/home/ec2-user/react-app'):
+
+            conn.run('pm2 start npm --name "react-app" -- start')
 	        #pull recent commits from github
             conn.run('git pull {}'.format(repo_url)) 
 	        # npm install
@@ -22,12 +25,13 @@ def deploy_to_ec2(repo_url):
             # build an optimized version of the app
             conn.run('npm run build')          
             # run the applicatino using pm2
-            conn.sudo('pm2 restart react-app')
+            conn.sudo('pm2 list')
 	    #when succeffsul print this message, you shall not fail me!
         print("Deployment completed successfully!")
 
     except Exception as e:
         print("Error:", e)
+    
 #initilize argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Deploy a React application to EC2.')
@@ -37,7 +41,6 @@ if __name__ == "__main__":
 
     # Trigger deployment funciotn
     deploy_to_ec2(args.repo_url)
-
 
 #https://docs.fabfile.org/en/latest/api/connection.html
 #https://stackoverflow.com/questions/27957373/python-import-and-initialize-argparse-after-if-name-main
